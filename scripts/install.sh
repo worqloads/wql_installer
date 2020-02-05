f9eae1c910cac790d2fbf8b24d92f78473e680d0
hnltcs <yi.jiang@hanalytics.fr>
2020-02-05 10:02:08 +0100

    re

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,10 +11,11 @@
 # Initialize variables
 app_folder="/app"
 scaler_folder="${app_folder}/scaler"
+installer_folder="${scaler_folder}/installer"
 secudir=${scaler_folder}/.keys
 git_user="hnltcs"
 wql_user=`whoami`
-log_file='./wql_installer.log'
+log_file="./wql_installer_$(date "+%Y.%m.%d-%H.%M.%S").log"
 # ####################################################
 
 # stop if there's an error
@@ -34,37 +35,38 @@ echo " + App version: $WQL_VERSION"
 
 # install NodeJS, NPM, PM2, GIT
 yes | sudo yum install curl git                                                                     &> ${log_file}
-yes | sudo yum remove -y nodejs npm                                                                 &> ${log_file}
-[[ -d ${app_folder} ]] || sudo mkdir -p ${app_folder}                                               &> ${log_file}
-sudo chown -R $wql_user:$wql_user ${app_folder}                                                     &> ${log_file}
-[[ -d ~/.ssh ]] || mkdir ~/.ssh && chmod 700  ~/.ssh                                                &> ${log_file}
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -                                     &> ${log_file}
-yes | sudo yum install -y nodejs                                                                    &> ${log_file}
-yes | sudo npm install npm@latest -g                                                                &> ${log_file}
-yes | sudo npm install pm2 -g                                                                       &> ${log_file}
-[[ -d ~/.npm ]] && sudo chown -R $wql_user:$wql_user ~/.npm                                         &> ${log_file}
-[[ -d ~/.config ]] && sudo chown -R $wql_user:$wql_user ~/.config                                   &> ${log_file}
+yes | sudo yum remove -y nodejs npm                                                                 &>> ${log_file}
+[[ -d ${app_folder} ]] || sudo mkdir -p ${app_folder}                                               &>> ${log_file}
+sudo chown -R $wql_user:$wql_user ${app_folder}                                                     &>> ${log_file}
+[[ -d ~/.ssh ]] || mkdir ~/.ssh && chmod 700  ~/.ssh                                                &>> ${log_file}
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -                                     &>> ${log_file}
+yes | sudo yum install -y nodejs                                                                    &>> ${log_file}
+yes | sudo npm install npm@latest -g                                                                &>> ${log_file}
+yes | sudo npm install pm2 -g                                                                       &>> ${log_file}
+[[ -d ~/.npm ]] && sudo chown -R $wql_user:$wql_user ~/.npm                                         &>> ${log_file}
+[[ -d ~/.config ]] && sudo chown -R $wql_user:$wql_user ~/.config                                   &>> ${log_file}
 
 # add cron housekeeping script of pm2 logs
-pm2 install pm2-logrotate              &> ${log_file}
-pm2 set pm2-logrotate:max_size 100M             &> ${log_file}
-pm2 set pm2-logrotate:compress true             &> ${log_file}
-pm2 set pm2-logrotate:rotateInterval '0 * * * *'              &> ${log_file}
+pm2 install pm2-logrotate              &>> ${log_file}
+pm2 set pm2-logrotate:max_size 100M             &>> ${log_file}
+pm2 set pm2-logrotate:compress true             &>> ${log_file}
+pm2 set pm2-logrotate:rotateInterval '0 * * * *'              &>> ${log_file}
 
 # if $scaler_folder already exists, do a backup
-[[ -d $scaler_folder ]] && sudo mv $scaler_folder "${scaler_folder}_$(date "+%Y.%m.%d-%H.%M.%S")"   &> ${log_file}
-git clone https://github.com/worqloads/wql_installer.git $scaler_folder                             &> ${log_file}
+[[ -d $scaler_folder ]] && sudo mv $scaler_folder "${scaler_folder}_$(date "+%Y.%m.%d-%H.%M.%S")"   &>> ${log_file}
+sudo rm -rf ${installer_folder}
+git clone https://github.com/worqloads/wql_installer.git $installer_folder                          &>> ${log_file}
 
-cd ${scaler_folder}
-[[ ! -z "$WQL_VERSION" ]] && git checkout ${WQL_VERSION}                                            &> ${log_file}
-sudo npm install                                                                                    &> ${log_file}
-[[ -d ${secudir} ]] || mkdir -p ${secudir}                                                          &> ${log_file}
-sudo chown -R $wql_user:$wql_user ${app_folder}                                                     &> ${log_file}
+cd ${installer_folder}
+[[ ! -z "$WQL_VERSION" ]] && git checkout ${WQL_VERSION}                                            &>> ${log_file}
+sudo npm install                                                                                    &>> ${log_file}
+[[ -d ${secudir} ]] || mkdir -p ${secudir}                                                          &>> ${log_file}
+sudo chown -R $wql_user:$wql_user ${app_folder}                                                     &>> ${log_file}
 
 # get aws instance region
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` &> ${log_file}
-[[ -z $TOKEN ]] || awsregion=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone) &&  echo ${awsregion::-1}  > ${scaler_folder}/.aws_region
-[[ -z $TOKEN ]] || curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/instance-id > ${scaler_folder}/.aws_instanceid
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` &>> ${log_file}
+[[ -z $TOKEN ]] || awsregion=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone) && echo ${awsregion::-1} > ${installer_folder}/.aws_region
+[[ -z $TOKEN ]] || curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id > ${installer_folder}/.aws_instanceid
 
 [[ -f .aws_region && -f .aws_instanceid ]] || exit 1
 # create local configuration
@@ -73,7 +75,9 @@ node register_min.js
 
 # registration successful
 if [[ $? -eq 0 && -f './conf.json' ]]; then
-    pm2 start scale_doer_check_min.js scale_doer_collect_min.js scale_doer_scale_min.js             &> ${log_file}
-    pm2 save
+    cd ${scaler_folder}
+    mv ${installer_folder}/*min.js ${installer_folder}/node_modules ${installer_folder}/.aws_* .    &>> ${log_file}
+    pm2 start scale_doer_check_min.js scale_doer_collect_min.js scale_doer_scale_min.js             &>> ${log_file}
+    pm2 save                                                                                        &>> ${log_file}
 fi
 
